e27dc7a4ae6bc863d175a992c9d6ebb6eb096213
Uchenna Oko <Uchenna.emma193@gmail.com>
2024-08-02 13:46:40 +0100

    Convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,21 @@ import { loadCart } from "../data/cart.js";
 // import '../data/backend-practice.js';
 // import '../data/cart-class.js';
 
+async function loadPage() {
+    await loadProductsFetch();
+
+    const value = await new Promise((resolve) => {
+        loadCart(() => {
+            resolve('value3');
+        });
+    });
+
+    renderOrderSummary();
+    renderPaymentSummary();
+}
+loadPage();
+
+/*
 Promise.all([
    loadProductsFetch(),
    new Promise((resolve) => {
@@ -18,6 +33,7 @@ Promise.all([
     renderOrderSummary();
     renderPaymentSummary();
 });
+*/
 
 
 /*
